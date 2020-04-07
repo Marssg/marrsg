3ac9138c45e3fa91e1c534a4c1be541094a2330f
Marssg <63108975+Marssg@users.noreply.github.com>
2020-04-07 22:20:05 +0200

    Create mesClasses.py

---
+++ mesClasses.py
@@ -0,0 +1,11 @@
+class User(object): # Définition de la classe utilisateur
+    """
+    Classe définissant un utilisateur et ses attributs
+    - nom = user name (type = str)
+    - wishlist = what this user would like (type = WishList)
+    - no_wishlist = what this user doesn't want (type = WishList)
+    """
+   
+    def __init__(self):
+        """on défini les valeurs par défaut des attributs"""
+        self.nom = nom
